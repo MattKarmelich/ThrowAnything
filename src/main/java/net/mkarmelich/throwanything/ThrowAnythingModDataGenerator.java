0c10d317e4afdb6daeae1ff0e199bc2b6c7ccefd
unknown <mattkarmelich@gmail.com>
2025-07-04 12:53:57 -0700

    init commit

---
+++ src/main/java/net/mkarmelich/throwanything/ThrowAnythingModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mkarmelich.throwanything;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ThrowAnythingModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
