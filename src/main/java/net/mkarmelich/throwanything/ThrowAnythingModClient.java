cb2003fe361580b372859d7f25c20522a6c92b4f
unknown <mattkarmelich@gmail.com>
2025-07-05 08:26:42 -0700

    take user input

--- src/main/java/net/mkarmelich/throwanything/ThrowAnythingModClient.java
+++ src/main/java/net/mkarmelich/throwanything/ThrowAnythingModClient.java
@@ -1,10 +1,12 @@
 package net.mkarmelich.throwanything;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.mkarmelich.throwanything.event.KeyInputHandling;
 
 public class ThrowAnythingModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        KeyInputHandling.register();
 
     }
 }
---
+++ src/main/java/net/mkarmelich/throwanything/event/KeyInputHandling.java
@@ -0,0 +1,34 @@
+package net.mkarmelich.throwanything.event;
+
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
+
+public class KeyInputHandling {
+    public static final String KEY_CATEGORY_INPUT = "key.category.input";
+    public static final String KEY_THROW_ITEM = "key.throw_item";
+
+    public static KeyBinding throwingKey;
+
+    public static void registerKeyInputs() {
+        ClientTickEvents.END_CLIENT_TICK.register(minecraftClient -> {
+            if(throwingKey.isPressed()) {
+                minecraftClient.player.sendMessage(Text.of("This is an item"), true);
+            }
+        });
+    }
+
+    public static void register() {
+        throwingKey = KeyBindingHelper.registerKeyBinding( new KeyBinding(
+                KEY_THROW_ITEM,
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_R,
+                KEY_CATEGORY_INPUT
+        ));
+
+        registerKeyInputs();
+    }
+}
